26c9c21489de40500ccc155d71ae920637b87617
Jessie Peng <jessie.peng2010@gmail.com>
2022-01-19 20:44:54 -0500

    added liking

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './header/Header'
+import PhotoCard from './photos/PhotoCard'
+import PhotoCards from './photos/PhotoCards'
+import React, { useState, useEffect } from "react";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#5f5fc4',
+      main: '#283593',
+      dark: '#001064',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#f8fdff',
+      main: '#c5cae9',
+      dark: '#9499b7',
+      contrastText: '#000',
+    },
+  },
+});
+
+
+
 
 function App() {
+  const [queryData, setQueryData] = useState('star');  
+  const currentSearch = search => {
+    setQueryData(search);
+  }
+
+  
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={theme}>
+      <Header search={currentSearch}/>
+      <PhotoCards query={queryData}/>
+      </ThemeProvider>
+      
+
+
     </div>
   );
 }
---
+++ src/photos/PhotoCards.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import Grid from '@mui/material/Grid';
+import FormLabel from '@mui/material/FormLabel';
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import RadioGroup from '@mui/material/RadioGroup';
+import Radio from '@mui/material/Radio';
+import Paper from '@mui/material/Paper';
+import PhotoCard from './PhotoCard';
+
+export default function PhotoCards(props) {
+  
+
+const [photoData, setPhotoData] = useState(null);
+
+useEffect(() => {
+    fetchPhoto();
+
+    async function fetchPhoto() {
+      const res = await fetch(
+                // we'll update the KEYHERE soon!
+        `https://images-api.nasa.gov/search?media_type=image&q=${props.query}`
+      );
+      const data = await res.json();
+      setPhotoData(data);
+    }
+  }, []);
+
+  if (!photoData) return <div />;
+  console.log(photoData);
+
+  function createCard(value, index) {
+      if (value.data[0].media_type != "video") {
+          return (<Grid key={index} item>
+          <PhotoCard href={value.links[0].href} 
+          title={value.data[0].title}
+          description={value.data[0].description}
+          date={value.data[0].date_created}/>
+        </Grid>);
+      } 
+  }
+
+  return (
+    <Grid sx={{ flexGrow: 1 }} container spacing={2} pt="50px" pl="15px" pr="15px">
+      <Grid item xs={12}>
+        <Grid container justifyContent="center" spacing={6}>
+          {photoData.collection.items.map((value, index) => createCard(value,index))}
+        </Grid>
+      </Grid>
+      <Grid item xs={12}>
+        <Paper sx={{ p: 2}}>
+          <Grid container>
+            <Grid item>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Grid>
+    </Grid>
+  );
+}
